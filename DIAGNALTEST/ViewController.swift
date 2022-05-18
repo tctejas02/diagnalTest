1a7a9de85082ab0293b862c1004a77257b587ee4
tejas <tejas.chaudhari@playboxtv.in>
2022-05-18 20:08:09 +0530

    Initial Commit

---
+++ DIAGNALTEST/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DIAGNALTEST
+//
+//  Created by tejas chaudhari on 18/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
